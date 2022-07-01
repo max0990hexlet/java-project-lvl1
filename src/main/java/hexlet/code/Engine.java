0c615156ed3b7713d9b2d5995af214b295e63b1a
max0990hexlet <grigorevmax0990@gmail.com>
2022-07-01 13:48:36 +0500

    fix "Let's try again"

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -24,7 +24,7 @@ public class Engine {
             } else {
                 System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + rightAnswer + "'.");
                 countRightAnswer = 0;
-                System.out.println("Let's try again, " + userName);
+                System.out.println("Let's try again, " + userName + "!");
                 return;
             }
         }
